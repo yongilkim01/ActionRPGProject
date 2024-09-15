74a7f876c84bac10e0ea9aff2b4cee02321f0031
yongilkim01 <yongil.k01@gmail.com>
2024-09-15 21:42:57 +0900

    fix: resolve bug related to player weapon equipping
    1. PlayerGameplayAbility.cpp
    -The issue in UPlayerGameplayAbility::GetPlayerCharacter() was that a weak pointer for the player was returning nullptr, and this has been fixed.
    
    Authored-by : Yongil Kim <yongil.k01@gmail.com>

--- Source/ActionRPGProject/Private/AbilitySystem/Abilities/PlayerGameplayAbility.cpp
+++ Source/ActionRPGProject/Private/AbilitySystem/Abilities/PlayerGameplayAbility.cpp
@@ -11,7 +11,7 @@ APlayerCharacter* UPlayerGameplayAbility::GetPlayerCharacter()
     // Player character 객체의 약한 포인터가 유효하지 않을 경우 즉 nullptr일 경우 타입 캐스트 후 할당
     if (!weakPlayerCharacter_.IsValid())
     {
-        Cast<APlayerCharacter>(CurrentActorInfo->AvatarActor);
+        weakPlayerCharacter_ = Cast<APlayerCharacter>(CurrentActorInfo->AvatarActor);
     }
     return weakPlayerCharacter_.IsValid() ? weakPlayerCharacter_.Get() : nullptr;
 }
--- Source/ActionRPGProject/Private/ActionRPGGameplayTags.cpp
+++ Source/ActionRPGProject/Private/ActionRPGGameplayTags.cpp
@@ -14,4 +14,7 @@ namespace ActionRPGGameplayTags
 
 	// Player tag
 	UE_DEFINE_GAMEPLAY_TAG(Player_Weapon_Axe, "Player.Weapon.Axe");
+
+	UE_DEFINE_GAMEPLAY_TAG(Player_Event_Equip_Axe, "Player.Event.Equip.Axe");
+	UE_DEFINE_GAMEPLAY_TAG(Player_Event_Unequip_Axe, "Player.Event.Unequip.Axe");
 }
